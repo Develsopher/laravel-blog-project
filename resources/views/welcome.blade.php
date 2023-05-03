72fddf0d17d929e06f4d307c6d885aae8a04646c
JunHyeongChae <junhyeong.chae@motiv-i.com>
2023-05-03 16:20:10 +0900

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        @vite('resources/js/app.js')
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body class="antialiased">
+        <h1 class="text-xl font-bold underline text-green-400">Hello Tailwind !</h1>
+    </body>
+</html>
