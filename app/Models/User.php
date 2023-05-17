1d51ebf90a9e672fa8993caa54b2d6a66d0535e2
JunHyeongChae <h_e_ll_o_@naver.com>
2023-05-17 23:33:38 +0900

    Update : [Profile]fallback이미지 추가 및 model accesor 추가

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Post::class, 'user_id');
     }
+
+    public function getAvatarAttribute($value)
+    {
+        return $value ? '/storage/avatars/'. $value : '/avatar-fallback.jpg';
+    }
 }
